6ed1a095bc0368472fcec309a3d76c8f1755af1b
root <root@srv1301602.hstgr.cloud>
2026-02-23 20:24:23 +0000

    Update controllers, routes, uploads handling, and admin/blog features

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/admin/",
   plugins: [react()],
-  server: {
-    port: 9999,
-    strictPort: true,
-  },
 });
